6732eedf99727026d51f9c42d18a9a8a839d311e
CapoRay0 <caporay0.0@gamil.com>
2021-08-04 23:47:02 +0800

    【調整】Default排版

--- 7302AccountingNote/7302AccountingNote/Default.aspx.cs
+++ 7302AccountingNote/7302AccountingNote/Default.aspx.cs
@@ -15,24 +15,26 @@ namespace _7302AccountingNote
         protected void Page_Load(object sender, EventArgs e)
         {
             //---------取得顯示資料的變數---------//
-            DataTable accessTime = GetTime(); //拿到dt
-            var accCount = GetTotalAcc(); //取得流水帳資料的數量
-            var idCount = GetUserCount(); //取得使用者的數量
+            DataTable accessTime = GetTime();   // 取得時間的DataTable
+            var accCount = GetTotalAcc();       // 取得流水帳資料的數量
+            var idCount = GetUserCount();       // 取得使用者的數量
 
-            //---------顯示資料----------//
-            this.ltFstRec.Text = accessTime.Rows[0][0].ToString();
-            this.ltLstRec.Text = accessTime.Rows[accessTime.Rows.Count-1][0].ToString();
-            this.ltTotalAcc.Text = accCount.ToString();
-            this.ltUserNum.Text = idCount.ToString();
+            //-------------顯示資料--------------//
+            this.ltFstRec.Text = accessTime.Rows[0][0].ToString(); // 初次記帳
+            this.ltLstRec.Text = accessTime.Rows[accessTime.Rows.Count-1][0].ToString(); // 最後記帳
+            this.ltTotalAcc.Text = accCount.ToString(); // 記帳數量
+            this.ltUserNum.Text = idCount.ToString(); // 會員數
         }
 
+        /// <summary> 取得時間 </summary>
+        /// <returns></returns>
         public static DataTable GetTime()
         {
             string connectionString = DBHelper.GetConnectionString();
             string dbCommandString = 
                 $@" SELECT 
                     [CreateDate]
-                FROM [7302AccountingNote].[dbo].[AccountingNote]
+                FROM [AccountingNote]
                 ORDER BY [CreateDate] ASC
                 ";
             List<SqlParameter> list = new List<SqlParameter>();
@@ -44,12 +46,13 @@ namespace _7302AccountingNote
             }
             catch (Exception ex)
             {
-                Logger.WriteLog(ex);   //利用logger存ex而不要用consloe  
+                Logger.WriteLog(ex);
                 return null;
             }
         }
 
-
+        /// <summary> 取得流水帳總數量 </summary>
+        /// <returns></returns>
         public static string GetTotalAcc()
         {
             string connectionString = DBHelper.GetConnectionString();
@@ -68,18 +71,20 @@ namespace _7302AccountingNote
             }
             catch (Exception ex)
             {
-                Logger.WriteLog(ex);   //利用logger存ex而不要用consloe  
+                Logger.WriteLog(ex);
                 return null;
             }
         }
 
+        /// <summary> 取得使用者總數量 </summary>
+        /// <returns></returns>
         public static string GetUserCount()
         {
             string connectionString = DBHelper.GetConnectionString();
             string dbCommandString = 
                 $@" SELECT
                         Count ([ID]) 
-                    FROM [7302AccountingNote].[dbo].[UserInfo]
+                    FROM [UserInfo]
                 ";
             List<SqlParameter> list = new List<SqlParameter>();
             try
@@ -90,11 +95,14 @@ namespace _7302AccountingNote
             }
             catch (Exception ex)
             {
-                Logger.WriteLog(ex);   //利用logger存ex而不要用consloe  
+                Logger.WriteLog(ex);
                 return null;
             }
         }
 
+        /// <summary> 導至登入頁面 </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void btnLogin_Click(object sender, EventArgs e)
         {
             Response.Redirect("/Login.aspx");
